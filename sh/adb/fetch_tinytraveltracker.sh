fe37dd61a23b5c32ae3761c4991865d850ef948c
Karl Semich <0xloem@gmail.com>
2016-03-16 09:30:22 -0400

    sh/adb: basics working

--- sh/adb/fetch_tinytraveltracker.sh
+++ sh/adb/fetch_tinytraveltracker.sh
@@ -5,14 +5,3 @@
 . "$LITELOGDIRSH"/adb/functions
 
 android_fetchall gps tinytraveltracker /sdcard/android/data/com.rareventure.gps2/files '^gps\.db3$' sqlite3
-
-PLAN:
-[ ] don't forget to add beepme data
-[ ] just store the sqlite3 file in tmp for now.  it'll get synced as a priority file for conversion
-[ ] copy from fetch.sh a basic function to relatively-correctly take a file from a connected phone and archive it
-[ ] non-sqlite3 files can be finalized
-  [ ] beepme
-  [ ] tinytraveltracker
-  [ ] voice recorder
-  [ ] muse
-
