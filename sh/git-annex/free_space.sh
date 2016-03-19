e12ab8abb50ccad84ec019759e7e4fbc634851bf
Karl Semich <gmkarl@gmail.com>
2016-03-19 19:42:07 -0400

    sh/git-annex: trust glacier

--- sh/git-annex/free_space.sh
+++ sh/git-annex/free_space.sh
@@ -4,7 +4,7 @@
 . "$LITELOGDIR"/sh/functions
 
 cd "$LOGDIR"
-flock .git_annex_lock -c 'git annex sync --content'
+flock .git_annex_lock -c 'git annex sync --content --trust-glacier'
 git annex unused --time-limit=20s
-git annex drop --unused --force --time-limit=20s
+git annex drop --trust-glacier --unused --force --time-limit=20s
 git annex drop --auto --numcopies 1 --time-limit=20s
