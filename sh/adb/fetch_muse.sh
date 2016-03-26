add682b5a3268c70f3c734568c0ac98c60dee2ff
Karl Semich <0xloem@gmail.com>
2016-03-26 15:05:05 -0400

    sh/adb: Fetch all musedata files, not just .muse

--- sh/adb/fetch_muse.sh
+++ sh/adb/fetch_muse.sh
@@ -4,4 +4,4 @@
 . "$LITELOGDIR"/sh/functions
 . "$LITELOGDIRSH"/adb/functions
 
-android_fetchall bio muse /sdcard/Download '\.muse$' raw delete
+android_fetchall bio muse /sdcard/Download '^musedata_' raw delete
