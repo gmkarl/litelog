65fb15c7a91ad3ecfdbff918e5ef244f6bcad2d9
Karl Semich <gmkarl@gmail.com>
2016-05-25 06:20:01 -0400

    sh/rtl-sdr: provide option to enable raw logging

--- sh/rtl-sdr/record.sh
+++ sh/rtl-sdr/record.sh
@@ -11,8 +11,11 @@ RTLPOWER_ARGS="${RTLPOWER_ARGS:- -f 0:1700M:250k -g 50 -i 45s}"
 
 
 date_now="$(format_date now)"
-export RTL_LOGFILE="$LOGDIR/$(get_logfilename_inprogress rtlsdr "$date_now" "$HOSTNAME" "$dev_serial"-logfile "raw" "raw")"
-CONSOLE_LOGFILE="$LOGDIR/$(get_logfilename_inprogress rtlsdr "$date_now" "$HOSTNAME" "$dev_serial"-console "raw" "txt")"
+if test -n "$RADIORAW"
+then
+	export RTL_LOGFILE="$LOGDIR/$(get_logfilename_inprogress rtlsdr "$date_now" "$HOSTNAME" "$dev_serial"-logfile "raw" "raw")"
+fi
+#CONSOLE_LOGFILE="$LOGDIR/$(get_logfilename_inprogress rtlsdr "$date_now" "$HOSTNAME" "$dev_serial"-console "raw" "txt")"
 POWER_LOGFILE="$LOGDIR/$(get_logfilename_inprogress rtlsdr "$date_now" "$HOSTNAME" "$dev_serial"-power "raw" "csv")"
 RTLPOWER_ARGS="rtl_power -d $dev_index -e $((ROTATE_SECONDS)) $RTLPOWER_ARGS"
 echo "> $RTLPOWER_ARGS $POWER_LOGFILE"
