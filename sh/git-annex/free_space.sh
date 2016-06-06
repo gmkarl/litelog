b043731d3323aec906e817d6a4bfb24491a26b58
Karl Semich <0xloem@gmail.com>
2016-06-06 16:48:53 -0400

    sh/git-annex: add GITANNEX_MOVETO to dump files to one repo when space is tight

--- sh/git-annex/free_space.sh
+++ sh/git-annex/free_space.sh
@@ -4,6 +4,22 @@
 . "$LITELOGDIR"/sh/functions
 
 cd "$LOGDIR"
-git annex unused --time-limit=20s
-git annex drop --trust-glacier --unused --force --time-limit=20s
-git annex drop --auto --numcopies 1 --time-limit=20s
+
+DAEMONPID="$(cat "$(git rev-parse --git-dir)"/annex/daemon.pid)"
+
+if ! ps "$DAEMONPID" > /dev/null
+then
+  # daemon not running
+  
+  # move files to a remote repo
+  if test -n "$GITANNEX_MOVETO"
+  then
+    git annex move --all --to="$GITANNEX_MOVETO"
+  fi
+fi
+
+# TODO: if size still tight, pack git history in the repo, then move git history to some remote repo
+
+#git annex unused --time-limit=20s
+#git annex drop --trust-glacier --unused --force --time-limit=20s
+#git annex drop --auto --numcopies 1 --time-limit=20s
