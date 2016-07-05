4e009e8e892d2cba77442456ec6d7156ae8e2eb8
Karl Semich <0xloem@gmail.com>
2016-07-05 16:38:04 -0400

    sh/muse: add library path for muse-io

--- sh/muse/record.sh
+++ sh/muse/record.sh
@@ -7,4 +7,6 @@ MUSE_DIR="${MUSE_DIR:-/opt/Muse}"
 
 MAC="$1"
 
+export LD_LIBRARY_PATH="$MUSE_DIR":"$LD_LIBRARY_PATH"
+
 "$MUSE_DIR"/muse-io --device "$MAC" --no-dsp --preset ae --osc-enable-dropped --osc-timestamp
