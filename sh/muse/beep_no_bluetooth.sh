77ea25ce3e11826a45875d0511be7be9e3747f8b
Karl Semich <gmkarl@gmail.com>
2016-08-04 04:02:30 -0400

    sh/muse: beep with alsa rather than pcspkr

--- sh/muse/beep_no_bluetooth.sh
+++ sh/muse/beep_no_bluetooth.sh
@@ -1,7 +1,34 @@
 #!/bin/sh
 
-. /etc/litelog
-. "$LITELOGDIR"/sh/functions
+if [ -e /etc/litelog ]
+then
+	. /etc/litelog
+	. "$LITELOGDIR"/sh/functions
+fi
+
+pcspkr_beep()
+{
+	beep
+}
+
+alsa_beep()
+{
+	beep_samples=8000
+	beep_hertz=800
+	samples_per_period=$((8000/beep_hertz))
+	periods_per_beep=$((beep_samples/samples_per_period))
+	for ((p=0; p<periods_per_beep; ++p))
+	do
+		for ((s=0; s<samples_per_period/2; ++s))
+		do
+			printf "\xff"
+		done
+		for ((s=0; s<samples_per_period/2; ++s))
+		do
+			printf "\x00"
+		done
+	done | aplay -
+}
 
 # beep if no incoming bluetooth packets
 A="$(hciconfig|grep RX)"
@@ -11,7 +38,7 @@ do
   B="$(hciconfig|grep RX)"
   if [ "$A" == "$B" ]
   then
-    beep
+    alsa_beep
     hciconfig
   fi
   A="$B"
