000cb512282384b1fe09bb86e33d1a24597b1f74
gmkarl@gmail.com <gmkarl@gmail.com>
2016-11-06 21:15:24 +0000

    check for write permissions before execing

--- sh/asciinema/enter.sh
+++ sh/asciinema/enter.sh
@@ -5,6 +5,13 @@ then
 	. /etc/litelog
 
 	. "$LITELOGDIR"/sh/functions
-	
-	exec asciinema rec -c "$(readlink -f /proc/$$/exe)" -q "$LOGDIR/$(get_logfilename_inprogress terminal "$(format_date now)" "$HOSTNAME" "$TERM$$" "raw" json)"
+
+	command="$(readlink -f /proc/$$/exe)"
+	output="$LOGDIR/$(get_logfilename_inprogress terminal "$(format_date now)" "$HOSTNAME" "$TERM$$" "raw" json)"
+
+	if touch "$output"
+	then
+		rm "$output"
+		exec asciinema rec -c "$command" -q "$output"
+	fi
 fi
