c21473b5cee7a7da196b2e1cd5268b8b9b15fc9b
gmkarl@gmail.com <gmkarl@gmail.com>
2016-11-06 12:49:33 +0000

    sh/asciinema: check shell for interactivity (PS1) before lauching

--- sh/asciinema/enter.sh
+++ sh/asciinema/enter.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-. /etc/litelog
-
-. "$LITELOGDIR"/sh/functions
-
-if ! test "$_LITELOG_SH_ASCIINEMA" = 1
+if test -n "$PS1" -a -z "$_LITELOG_SH_ASCIINEMA"
 then
+	. /etc/litelog
+
+	. "$LITELOGDIR"/sh/functions
+
 	export _LITELOG_SH_ASCIINEMA=1
 	
 	exec asciinema rec -c "$(readlink -f /proc/$$/exe)" -q "$LOGDIR/$(get_logfilename_inprogress terminal "$(format_date now)" "$HOSTNAME" "$TERM$$" "raw" json)"
