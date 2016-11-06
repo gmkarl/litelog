39c056e81854fdb10bd0791c8a224da4d57a9d09
gmkarl@gmail.com <gmkarl@gmail.com>
2016-11-06 17:49:15 +0000

    sh/asciinema: re-use asciinema env var to decrease pollution

--- sh/asciinema/enter.sh
+++ sh/asciinema/enter.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-if test -n "$PS1" -a -z "$_LITELOG_SH_ASCIINEMA"
+if test -n "$PS1" -a -z "$ASCIINEMA_REC"
 then
 	. /etc/litelog
 
 	. "$LITELOGDIR"/sh/functions
-
-	export _LITELOG_SH_ASCIINEMA=1
 	
 	exec asciinema rec -c "$(readlink -f /proc/$$/exe)" -q "$LOGDIR/$(get_logfilename_inprogress terminal "$(format_date now)" "$HOSTNAME" "$TERM$$" "raw" json)"
 fi
