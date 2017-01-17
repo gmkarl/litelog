614a045c5c574ded35cdf51b50dc085c61ca9bf3
Karl Semich <0xloem@gmail.com>
2017-01-17 02:21:12 +0000

    sh/zeo: bugfix, ignore EOF

--- sh/zeo/dumpstream.sh
+++ sh/zeo/dumpstream.sh
@@ -11,5 +11,5 @@ serial="$(cat /sys/dev/char/$major:$minor/device/../../serial)"
 file="$LOGDIR/$(get_logfilename_inprogress bio "$(format_date now)" "$serial" zeo raw dump)"
 
 # The serial port is set at baud 38400, no parity, one stop bit.
-stty -F "$device" 38400 -parenb -cstopb
+stty -F "$device" 38400 -parenb -cstopb eof undef intr undef quit undef stop undef -brkint
 cat "$device" > "$file"
