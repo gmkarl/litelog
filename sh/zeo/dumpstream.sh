af96e935819c2b6d35e54a186b9eccf89d42045f
user <user@annex>
2017-03-24 17:49:44 -0400

    missing zeo fix vs terminated cat process

--- sh/zeo/dumpstream.sh
+++ sh/zeo/dumpstream.sh
@@ -8,8 +8,10 @@ major=$((0x$(stat -c %t "$device")))
 minor=$((0x$(stat -c %T "$device")))
 serial="$(cat /sys/dev/char/$major:$minor/device/../../serial)"
 
-file="$LOGDIR/$(get_logfilename_inprogress bio "$(format_date now)" "$serial" zeo raw dump)"
 
 # The serial port is set at baud 38400, no parity, one stop bit.
 stty -F "$device" 38400 -parenb -cstopb
-cat "$device" > "$file"
+while [ -r "$device" ]; do
+	file="$LOGDIR/$(get_logfilename_inprogress bio "$(format_date now)" "$serial" zeo raw dump)"
+	cat "$device" > "$file"
+done
