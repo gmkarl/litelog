28db7ce5584694127de2987ede227bff5fa28ddf
gmkarl@gmail.com <gmkarl@gmail.com>
2018-03-28 18:58:56 +0000

    sh/soapy_power: fix args

--- sh/soapy_power/record.sh
+++ sh/soapy_power/record.sh
@@ -11,4 +11,4 @@ scrubbedFMT="$(echo "$SOAPY_POWER_FMT" | sed 's/_/-/g')"
 
 output="$LOGDIR/$(get_logfilename_inprogress sdr "$(format_date now)" "$HOSTNAME" "soapypower-$scrubbedDEV" "raw" "$scrubbedFMT")"
 
-exec soapy_power "$SOAPY_POWER_ARGS" -O "$output" -F "$SOAPY_POWER_FMT" -e "$ROTATE_SECONDS" -d "$driverstr"
+exec soapy_power $SOAPY_POWER_ARGS -O "$output" -F "$SOAPY_POWER_FMT" -e "$ROTATE_SECONDS" -d "$driverstr"
